1f5ef20f926f925dc28f873a97ed8a9c06f91e56
frederick <frederickfrimpong284@gmail.com>
2026-02-07 21:04:32 +0000

    feat: add shadow effect to AlertsWindow and DeliveryPerformance components

--- src/components/dashboard/AlertsWindow.tsx
+++ src/components/dashboard/AlertsWindow.tsx
@@ -1,6 +1,6 @@
 export default function AlertsWindow() {
   return (
-    <div className="w-full p-4 border border-border rounded-md h-76">
+    <div className="w-full p-4 border border-border rounded-md h-76 shadow-xs">
       AlertsWindow
     </div>
   )
--- src/components/dashboard/DeliveryPerformance.tsx
+++ src/components/dashboard/DeliveryPerformance.tsx
@@ -1,6 +1,6 @@
 export default function DeliveryPerformance() {
   return (
-    <div className="w-full p-4 h-76 border border-border rounded-md">
+    <div className="w-full p-4 h-76 border border-border rounded-md shadow-xs">
       DeliveryPerformance
     </div>
   )
